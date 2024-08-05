d02d368cd6d5314b4c29c8ad822c42e437fbce73
mjavacam2010 <mjavacam2010@gmail.com>
2024-08-05 05:14:53 +0000

    feat: Adds a general error boundary component

---
+++ app/components/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+// app/components/ErrorBoundary.tsx
+import { useRouteError, isRouteErrorResponse } from "@remix-run/react";
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className="error-container">
+        <h1>
+          {error.status} {error.statusText}
+        </h1>
+        <p>{error.data}</p>
+      </div>
+    );
+  } else if (error instanceof Error) {
+    return (
+      <div className="error-container">
+        <h1>Error</h1>
+        <p>{error.message}</p>
+      </div>
+    );
+  } else {
+    return <h1>Unknown Error</h1>;
+  }
+}
